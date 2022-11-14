5f66ce475e50d854aab54af76ac7f4492c110552
AIMH9999 <104711708+AIMH9999@users.noreply.github.com>
2022-11-14 17:21:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
-import time
-url = 'https://www.lazada.vn/products/adidas-originals-giay-superstar-nam-mau-trang-gz3413-i1809316659-s8144103383.html?mp=1&spm=a2o4n.seller.list.i40.10e034e6lKOEx6'
+import urllib.request
 
+url = 'https://www.lazada.vn//products/i1240264949-s4621963744.html?spm=a2o4n.cart.0.0.4e0a705boaAJQK&urlFlag=true'
 
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "html.parser")
+def LAZ():
+    source = urllib.request.urlopen(url).read()
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    prices = soup.find_all('span', {'class': 'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl'})[0].text
+    buy = soup.find_all('span', {'class': 'pdp-button-text'})[0].text
+    return prices,buy
 
-prices = soup.find_all('div',{'id':'module_product_price_1'})[0].find('span').text
-buy = soup.find_all('div',{'id':'module_add_to_cart'})[0].find('span').text
-trangthai = ("Mua ngay")
-
-if (buy == trangthai):
-    print(url,buy)
-
-if (buy != trangthai):
-    while (True):
-        if (buy != trangthai):
-            print("...")
-            time.sleep(2)
-
-
-
+while(True):
+     print(LAZ(),url)
+     time.sleep(1)
