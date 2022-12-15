c47a1febf091ad4b43bb7b60a9e7ba9afa782cd4
Nguyễn Thành Long <104711708+AIMH9999@users.noreply.github.com>
2022-12-15 19:27:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from bs4 import BeautifulSoup
 import requests
 import urllib.request
 
-url = 'https://www.lazada.vn//products/i1240264949-s4621963744.html'
+url = 'https://www.timeanddate.com/worldclock/vietnam/hanoi'
 
-def LAZ():
+def THOI_GIAN():
     source = urllib.request.urlopen(url).read()
     result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
-    prices = soup.find_all('span', {'class': 'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl'})[0].text
-    buy = soup.find_all('span', {'class': 'pdp-button-text'})[0].text
-    return prices,buy
+    time = soup.find_all('span', {'class': 'h1'})[0].text
+    date = soup.find_all('span', {'id': 'ctdat'})[0].text
+    return time,date
 
 while(True):
-     print(LAZ(),url)
-     time.sleep(1)
+     print(THOI_GIAN())
+     time.sleep(0.3)
