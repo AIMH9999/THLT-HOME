af7dea64fc14cd3bef29bae001633eda7250aba4
Nguyễn Thành Long <104711708+AIMH9999@users.noreply.github.com>
2022-12-16 23:26:46 +0700

    Update DICH.cpp

--- DICH.cpp
+++ DICH.cpp
@@ -8,6 +8,7 @@
 //chọn kiểu dịch nhập từ bàn phím 1 hoặc 2
 loop:   printf("\n#Chon kieu nhap((1) la dich tu chu, so va ky tu sang he thap phan ASCII, (2) la dich tu he thap phan ASCII sang chu, so va ky tu):1/2 ");
         scanf("%c%c", &o, &m);
+// biến m dùng để loại bỏ thao tác nhập enter để không hiển thị giá trị 10 trong switch
         switch(o)
         {
 // dich từ chữ, số và kí tự sang mã thập phân ASCII
@@ -15,8 +16,10 @@ loop:   printf("\n#Chon kieu nhap((1) la dich tu chu, so va ky tu sang he thap p
                printf("nhap chu va ky tu: ");
                for(n=1; n<100000; n=n+1){
                    scanf("%c", &xn);
+// điều kiện if để không hiển thị giá trị 10 ở cuối hàng khi bấm enter		       
                    if(xn!=10)
                       printf("%u ", xn);
+// sau khi nhập chữ, số và kí tự bấm enter để dich từ chữ, số và kí tự sang mã thập phân ASCII và hiển thị ra màn hình và trở lại phần chọn kiểu nhập
                    else 
 		     goto loop;
                }
@@ -26,6 +29,7 @@ loop:   printf("\n#Chon kieu nhap((1) la dich tu chu, so va ky tu sang he thap p
                for(n=1; n<100000; n=n+1){
                   scanf("%u%c", &xn, &xn1);
                   printf("%c", xn);
+// điều kiện if để sau khi nhập mã thập phân ASCII bấm enter để dich từ mã thập phân ASCII sang chữ, số và kí tự và hiển thị ra màn hình và trở lại phần chọn kiểu nhập		       
                   if(xn1==10)
                     goto loop;
                }
